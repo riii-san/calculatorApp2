3a169514d3c5a2ccefd3dbc245f21c26b8eca501
ryoya <ryoya47@gmail.com>
2023-04-19 21:48:55 +0900

    Initial Commit

---
+++ calculatorApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculatorApp2
+//
+//  Created by 中野稜也 on 2023/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculatorApp2/calculatorApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp2App.swift
+//  calculatorApp2
+//
+//  Created by 中野稜也 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
